b05824f2dbec0cb46f43bc7bb173a81d810e3bbb
jitendra <jitendra@innowrap.com>
2024-09-25 09:55:06 +0530

    Update: image config for external images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 module.exports = {
     images: {
-      domains: ['innowrap.s3.ap-south-1.amazonaws.com'],
+      domains: [
+        'innowrap.s3.ap-south-1.amazonaws.com', 
+        'tpc-prod-static-files.s3.ap-south-1.amazonaws.com'
+      ],
     },
   };
   
