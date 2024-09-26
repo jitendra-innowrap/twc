853dc6bb8b6aff660fd1492508069ac7db04db1d
jitendra <jitendra@innowrap.com>
2024-09-26 14:47:35 +0530

    Updated: Added GTM and Google analytics

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -38,6 +38,49 @@ const Layout = ({ children, classList, parent, subLink, sub, subChild, noBreadcr
                 <meta property='og:image' content={`${process.env.NEXT_PUBLIC_BASE_URL}/layout/og-image.png`} />
                 <link rel="icon" href="/favicon.ico" />
                 <style>@import url('https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&family=Spartan:wght@300;400;500;600;700&display=swap');</style>
+
+
+
+
+                {/* Google Tag Manager */}
+                <script
+                
+                dangerouslySetInnerHTML={{
+                    __html: `
+                    (function(w,d,s,l,i){
+                        w[l]=w[l]||[];w[l].push({'gtm.start':
+                        new Date().getTime(),event:'gtm.js'});
+                        var f=d.getElementsByTagName(s)[0],
+                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+                        j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                        f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','GTM-MHGVLPLH');
+                    `,
+                }}
+                />
+                {/* Google Tag Manager (noscript) */}
+                <noscript>
+                <iframe
+                    src="https://www.googletagmanager.com/ns.html?id=GTM-MHGVLPLH"
+                    height="0"
+                    width="0"
+                    style={{ display: 'none', visibility: 'hidden' }}
+                />
+                </noscript>
+
+                {/* Google Analytics */}
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-VFQPJ2J8ND"></script>
+                <script
+                dangerouslySetInnerHTML={{
+                    __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-VFQPJ2J8ND');
+                    `,
+                }}
+                />
+
             </Head>
 
             {isToggled && <div className="body-overlay-1" onClick={toggleClick}></div>}
